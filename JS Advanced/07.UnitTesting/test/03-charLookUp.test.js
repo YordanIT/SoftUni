eb55e095213b759928eb9c38cda1d27000a1d120
Yordan Ivanov <yordangoshovivanov@gmail.com>
2022-06-11 19:11:11 +0200

    Update 03-charLookUp.test.js

--- JS Advanced/07.UnitTesting/test/03-charLookUp.test.js
+++ JS Advanced/07.UnitTesting/test/03-charLookUp.test.js
@@ -1,21 +1,20 @@
 const charLookUp = require('../03-charLookUp');
 const { assert } = require('chai');
-const { string } = require('yargs');
 
 describe('Test find char at given index', () => {
-    it('Should return undefined when test non string', () => {
+    it('Should return undefined if test non-string', () => {
         assert.equal(undefined, charLookUp(undefined, 5));
     })
 
-    it('Should return undefined when test NaN index', () => {
+    it('Should return undefined if test NaN index', () => {
         assert.equal(undefined, charLookUp('cat', 'index'));
     })
 
-    it('Should return message when test negative index', () => {
+    it('Should return message if test negative index', () => {
         assert.equal('Incorrect index', charLookUp('cat', -1));
     })
 
-    it('Should return message when test out of string length index', () => {
+    it('Should return message if test out of string length index', () => {
         assert.equal('Incorrect index', charLookUp('cat', 11));
     })
 

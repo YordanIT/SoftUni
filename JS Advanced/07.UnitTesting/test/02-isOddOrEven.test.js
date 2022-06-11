74c9147e8c842313598e09dc73dc6c2de99d49bf
Yordan Ivanov <yordangoshovivanov@gmail.com>
2022-06-11 18:30:48 +0200

    Update 02-isOddOrEven.test.js

--- JS Advanced/07.UnitTesting/test/02-isOddOrEven.test.js
+++ JS Advanced/07.UnitTesting/test/02-isOddOrEven.test.js
@@ -2,15 +2,15 @@ const isOddOrEven = require('../02-isOddOrEven');
 const { assert } = require('chai');
 
 describe('Test odd or even string length', () => {
-    it('Shoud return undefined when nonstring is tested', () => {
+    it('Should return undefined if nonstring is tested', () => {
         assert.equal(undefined, isOddOrEven(undefined));
     })
 
-    it('Shoud return even when even string passed', () => {
+    it('Should return even if even string passed', () => {
         assert.equal('even', isOddOrEven('cats'));
     })
 
-    it('Shoud return odd when odd string passed', () => {
+    it('Should return odd if odd string passed', () => {
         assert.equal('odd', isOddOrEven('cat'));
     })
 })
